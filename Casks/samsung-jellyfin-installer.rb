5569db8344302662f2bbe1762bdf8afb4966e04f
Anka <runner@iad20-gt1020-8cc6e7c5-d7b0-4936-931f-a19842ddced5-7633B46E8438.local>
2026-06-04 13:51:11 +0000

    samsung-jellyfin-installer 2.3.1.1

--- Casks/samsung-jellyfin-installer.rb
+++ Casks/samsung-jellyfin-installer.rb
@@ -1,13 +1,13 @@
 cask "samsung-jellyfin-installer" do
-  version "2.3.1.0"
+  version "2.3.1.1"
 
   on_arm do
-    sha256 "02c114898883255d50636f41e9018f6477565b8256a9f64d44f7be6269503135"
+    sha256 "61d98246416a551ad146b85b64e54a1d48b8b9fe1a8a0c226631a597e7037d39"
 
     url "https://github.com/Jellyfin2Samsung/Samsung-Jellyfin-Installer/releases/download/v#{version}/Jellyfin2Samsung-v#{version}-macos-arm64.dmg"
   end
   on_intel do
-    sha256 "3d5e3307ea90717d6512626ad9526c50f160cae767e3a9347d5d80a44ee6dd9a"
+    sha256 "701e094d43715f14ae02e1a2d362759efa7546f7a35f80d302812e1b93f92761"
 
     url "https://github.com/Jellyfin2Samsung/Samsung-Jellyfin-Installer/releases/download/v#{version}/Jellyfin2Samsung-v#{version}-macos-x64.dmg"
   end
