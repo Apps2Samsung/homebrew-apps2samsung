797bb7dfb4dfe9cbd99050ea38ecb249a48511e2
E <2974895+eduardomozart@users.noreply.github.com>
2026-07-03 20:59:00 -0300

    Fix bump-cask workflow and update apps2samsung hashes

--- Casks/apps2samsung.rb
+++ Casks/apps2samsung.rb
@@ -2,12 +2,12 @@ cask "apps2samsung" do
   version "2.5.7"
 
   on_arm do
-    sha256 "e2d100c8b8ac64149e8c3e8af44a639db8740f6d593b55d30e36b00f12418c67"
+    sha256 "0fbddca99988671a99053eed33960104c6178becec3480b3b0963f3123505329"
 
     url "https://github.com/Apps2Samsung/Apps2Samsung/releases/download/v#{version}/Apps2Samsung-v#{version}-macos-arm64.dmg"
   end
   on_intel do
-    sha256 "8811ca2db44f76d24aae3595bab670d87c35ce1acd89959b802281c8dda6df00"
+    sha256 "5eada0d9333f7d665b094172f7b8931970f2960ba369f9628d4331d5dac882c3"
 
     url "https://github.com/Apps2Samsung/Apps2Samsung/releases/download/v#{version}/Apps2Samsung-v#{version}-macos-x64.dmg"
   end
